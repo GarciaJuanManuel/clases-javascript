76986db699c25ea180120ce3b3e4260c7a752844
juan manuel garcia <juanmanug97@gmail.com>
2024-06-10 22:34:53 -0300

    clase de variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+let nombre = "juanma"; // LET - se pueden cambiar
+const edad = 27; // CONST - no se puede cambiar el valor de la variable
+const dni = 40011570;
+
+console.log(nombre);
+
+//let variablePrompt = prompt("por favor ingresar un numero")
+
+// console.log(variablePrompt);
+
+let respuesta = "bien y vos?";
+
+console.log("hola " + nombre + " como estas?");
